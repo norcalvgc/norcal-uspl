666047c800a02e2ce7c6098ffc8439e1ff08fedb
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2025-01-20 00:26:42 -0800

    Add Week 3 matches

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,6 +97,51 @@ export default function Home() {
         </section>
 
         <section className="w-full">
+          <h2 className="text-xl md:text-3xl font-bold mb-6 text-center bg-gray-900 text-white py-3 rounded-t-lg">Week 3 vs. Michigan</h2>
+          <div className="bg-white shadow-md rounded-b-lg p-6 hover:shadow-lg transition-shadow">
+          <p className="text-gray-600 text-sm mb-4">Dates: 1/20/25 - 1/26/25</p>
+            <div className="divide-y divide-gray-200">
+              <MatchRow 
+                norcalPlayer="MissingNoL"
+                norcalScore={0}
+                opponentPlayer="kamikazekidgc"
+                opponentScore={0}
+              />
+              <MatchRow 
+                norcalPlayer="Bobjoecarl"
+                norcalScore={0}
+                opponentPlayer="itachivgc"
+                opponentScore={0}
+              />
+              <MatchRow 
+                norcalPlayer="eragon"
+                norcalScore={0}
+                opponentPlayer="misspeller"
+                opponentScore={0}
+              />
+              <MatchRow 
+                norcalPlayer="turboisonline"
+                norcalScore={0}
+                opponentPlayer="izzywooperstan"
+                opponentScore={0}
+              />
+              <MatchRow 
+                norcalPlayer="kotoripoke"
+                norcalScore={0}
+                opponentPlayer="aydenbarber_"
+                opponentScore={0}
+              />
+              <MatchRow 
+                norcalPlayer="ToastNoButter"
+                norcalScore={0}
+                opponentPlayer="trunksshe"
+                opponentScore={0}
+              />
+            </div>
+          </div>
+        </section>
+
+        <section className="w-full">
           <h2 className="text-xl md:text-3xl font-bold mb-6 text-center bg-gray-900 text-white py-3 rounded-t-lg">Week 2 vs. Indiana</h2>
           <div className="bg-white shadow-md rounded-b-lg p-6 hover:shadow-lg transition-shadow">
           <p className="text-gray-600 text-sm mb-4">Dates: 1/13/25 - 1/19/25</p>
